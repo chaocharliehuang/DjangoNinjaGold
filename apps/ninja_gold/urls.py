2d38137ca71297c9e1e8b509fd8efff0ac68f19a
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-16 19:49:33 -0700

    submitting form at each place goes to a custom route, adds/subtracts appropriate golds, and updates activities

--- apps/ninja_gold/urls.py
+++ apps/ninja_gold/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^process_money/(?P<place_id>[0-9]+)$', views.process)
 ]
